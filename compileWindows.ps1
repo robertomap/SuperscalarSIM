7d6b32304c66d9d54885cf6a26846fb3c31ce995
axell-brendow <breno.axel@gmail.com>
2019-10-28 19:41:51 -0300

    Fixes a bug in the background image and in the PowerShell scripts

--- compileWindows.ps1
+++ compileWindows.ps1
@@ -1,4 +1,4 @@
 $javac_exe="$( (gc path_to_JDK_executables.txt)[0] )"
 
-mkdir bin
+mkdir bin 2> $null
 & "$javac_exe" -classpath src -d bin src/SuperscalarSIM/pkg1/*.java
--- runWindows.ps1
+++ runWindows.ps1
@@ -1,3 +1,4 @@
 $java_exe="$( (gc path_to_JDK_executables.txt)[1] )"
 
+mkdir bin 2> $null
 & "$java_exe" -classpath bin SuperscalarSIM.pkg1.Main
