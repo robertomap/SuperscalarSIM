f5cc5fb07a7779745b72d699a76d2785263d22fd
axell-brendow <breno.axel@gmail.com>
2019-10-28 20:08:14 -0300

    Make run script compile first

--- compileLinux.sh
+++ compileLinux.sh
@@ -1,3 +1,4 @@
 javac_exe=$(sed '1!d' path_to_JDK_executables.txt)
 
+mkdir bin
 $javac_exe -classpath src -d bin src/SuperscalarSIM/pkg1/*.java
--- runLinux.sh
+++ runLinux.sh
@@ -1,3 +1,4 @@
 java_exe=$(sed '2!d' path_to_JDK_executables.txt)
 
+./compileLinux.sh
 java -classpath bin SuperscalarSIM.pkg1.Main
