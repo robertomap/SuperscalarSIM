067942eb78d33f854554895f91836080e678573a
axell-brendow <breno.axel@gmail.com>
2019-10-28 21:03:31 -0300

    Removes some comments

--- buildWindows.ps1
+++ buildWindows.ps1
@@ -1,21 +1,24 @@
 $java_exe="$( (gc path_to_JDK_executables.txt)[1] )"
 $jar_exe="$( (gc path_to_JDK_executables.txt)[2] )"
 
-# Compila o código
 echo "Compiling the code ..."
+
 ./compileWindows.ps1
 
-# Cria o manifesto
+
 echo "Creating manifest.mf ..."
+
 echo "Main-Class: SuperscalarSIM.pkg1.Main
 Class-Path: bin/" | Out-File -Encoding Default manifest.mf
 
-# Gera o .jar
+
 echo "Generating .jar file ..."
+
 & "$jar_exe" vcfm SuperscalarSIM.jar manifest.mf -C bin/ .
 
-# Executa-o
+
 echo "Executing ..."
+
 pushd bin
 & "$java_exe" -jar ../SuperscalarSIM.jar
 popd
