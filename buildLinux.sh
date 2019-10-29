a026b69af85ee024e0dd84a23a9092980b383a03
axell-brendow <breno.axel@gmail.com>
2019-10-28 21:03:31 -0300

    Removes some comments

--- buildLinux.sh
+++ buildLinux.sh
@@ -1,21 +1,24 @@
 java_exe=$(sed '2!d' path_to_JDK_executables.txt)
 jar_exe=$(sed '3!d' path_to_JDK_executables.txt)
 
-# Compila o código
 echo "Compiling the code ..."
+
 ./compileLinux.sh
 
-# Cria o manifesto
+
 echo "Creating manifest.mf ..."
+
 echo "Main-Class: SuperscalarSIM.pkg1.Main
 Class-Path: bin/" > manifest.mf
 
-# Gera o .jar
+
 echo "Generating .jar file ..."
+
 "$jar_exe" vcfm SuperscalarSIM.jar manifest.mf -C bin/ .
 
-# Executa-o
+
 echo "Executing ..."
+
 pushd bin
 "$java_exe" -jar ../SuperscalarSIM.jar
 popd
